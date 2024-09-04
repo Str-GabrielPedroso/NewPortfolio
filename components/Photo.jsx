1e6822e8a28b5b2dbb65f2792f80036ee01bd3c6
Gabriel Pedroso <gbpedroso.22@gmail.com>
2024-09-04 19:34:12 -0300

    components/Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import profileImg from "../public/assets/profile.png"
 
 const Photo = () => {
 
@@ -14,7 +13,7 @@ const Photo = () => {
             >
                 <div className="w-[200px] h-[200px] xl:w-[360px] xl:h-[360px]">
                     <Image
-                        src={profileImg}
+                        src="/assets/prifile.png"
                         priority
                         quality={100}
                         fill
