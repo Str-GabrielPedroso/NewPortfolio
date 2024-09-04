08f5b819770a824f99b07ee602c2e5e7bb633c65
Gabriel Pedroso <gbpedroso.22@gmail.com>
2024-09-04 00:29:24 -0300

    correcao img

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,19 +1,27 @@
 "use client";
 
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Photo = () => {
+    const profileURL = "/assets/profile.png";
+
     return (
         <div className="w-full h-full relative">
-            <div
+            <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, transition: { delay: 1.8, duration: 0.3, ease: "easeInOut"}}}
             >
                 <div className="w-[200px] h-[200px] xl:w-[360px] xl:h-[360px]">
-                    <img
-                        src="/assets/profile.png"
+                    <Image
+                        src={profileURL}
+                        priority
+                        quality={100}
+                        fill
                         className="rounded-full border-4 border-primary outline outline-4 outline-accent grayscale-30"
                     />
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
